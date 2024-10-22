97db054ff1ec6256463341e0dd7c0f2f051fd7d7
MK <99416359+MK316@users.noreply.github.com>
2024-10-22 13:06:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,6 @@ ipa_data = {
     'w': {'Voicing': 'voiced', 'Place': 'labio-velar', 'Manner': 'approximant','Oro-nasal': '(oral)','Centrality':'(central)'}
 }
 
-
-
 def select_random_symbol():
     """Select a random IPA symbol"""
     symbol = random.choice(list(ipa_data.keys()))
@@ -54,7 +52,6 @@ if 'new_symbol' not in st.session_state or st.button("Start Quiz"):
     st.session_state.correct_count = 0
     st.session_state.attempts = 0
     st.session_state.current_symbol, st.session_state.current_data = select_random_symbol()
-    st.session_state.submit_pressed = False
 
 if "current_symbol" in st.session_state:
     st.write(f"IPA Symbol: {st.session_state.current_symbol}")
@@ -72,19 +69,19 @@ if "current_symbol" in st.session_state:
     with col5:
         centrality = st.radio("Centrality", ['(central)', 'lateral', '(not applicable)'], key=f"centrality_{st.session_state.attempts}")
 
-    if st.button("Submit"):
-        correct, _ = validate_selections(st.session_state.current_symbol, voicing, place, manner, oronasal, centrality)
-        if correct:
-            st.success("Correct!")
-            st.session_state.correct_count += 1
-        else:
-            st.error("Incorrect!")
-        st.session_state.attempts += 1
-        st.session_state.current_symbol, st.session_state.current_data = select_random_symbol()
-        st.session_state.submit_pressed = True
+    submit_col, continue_col = st.columns([1, 1])
+    with submit_col:
+        if st.button("Submit"):
+            correct, _ = validate_selections(st.session_state.current_symbol, voicing, place, manner, oronasal, centrality)
+            if correct:
+                st.success("Correct!")
+                st.session_state.correct_count += 1
+            else:
+                st.error("Incorrect!")
+            st.session_state.attempts += 1
+            st.session_state.current_symbol, st.session_state.current_data = select_random_symbol()
 
-if st.button("Continue"):
-    st.write(f"Final Score: {st.session_state.correct_count} out of {st.session_state.attempts}")
-    if st.session_state.submit_pressed:
-        st.session_state.new_symbol = True  # Refresh the session for a new start
-        st.session_state.submit_pressed = False
+    with continue_col:
+        if st.button("Continue"):
+            st.write(f"Current Score: {st.session_state.correct_count} out of {st.session_state.attempts}")
+
